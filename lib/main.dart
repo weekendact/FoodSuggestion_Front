4e097c54fa22c95c524e0741b958d1b3f608e23a
성현 <jshsky04@naver.com>
2023-11-17 10:50:06 +0900

    main -> home bottomnavigation

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,14 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:object_food_suggestion_project/Sign_Up_Page.dart';
-import 'package:object_food_suggestion_project/home.dart'; // http 패키지 import
+import 'pages.dart';
 
 void main() {
   runApp(MaterialApp(
     home: LoginScreen(),
   ));
 }
+
 class LoginScreen extends StatefulWidget {
   @override
   _LoginScreenState createState() => _LoginScreenState();
@@ -17,7 +18,6 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   TextEditingController _emailController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
-
 
   @override
   Widget build(BuildContext context) {
@@ -55,9 +55,8 @@ class _LoginScreenState extends State<LoginScreen> {
                 String email = _emailController.text;
                 String password = _passwordController.text;
 
-                // 로그인 요청을 보내고 응답 처리
                 var response = await http.post(
-                  Uri.parse('http://192.168.56.1:8080/login'), // 로그인 엔드포인트 URL
+                  Uri.parse('http://192.168.56.1:8080/login'),
                   headers: <String, String>{
                     'Content-Type': 'application/json; charset=UTF-8',
                   },
@@ -68,36 +67,197 @@ class _LoginScreenState extends State<LoginScreen> {
                 );
 
                 if (response.statusCode == 200) {
-                  // 로그인 성공
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => HomePage()),
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => MyApp()),
                   );
-                }
-                else {
-                  // 로그인 실패
+                } else {
                   print('로그인 실패.');
                 }
               },
               child: Text('로그인'),
             ),
             SizedBox(height: 10),
-
-        ElevatedButton(
-          onPressed: () {
-            // 회원가입 버튼 클릭 시 회원가입 화면으로 이동
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => SignUpPage()),
-            );
-          },
-          child: Text('회원가입'),
-        ),
-
-
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SignUpPage()),
+                );
+              },
+              child: Text('회원가입'),
+            ),
           ],
         ),
       ),
     );
   }
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MainScreen();
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 1;
+
+  final List<Widget> _pages = [
+    MyInfoPage(),
+    HomePage(),
+    SettingPage(),
+  ];
+
+  void refresh(int newIndex) {
+    setState(() {
+      _currentIndex = newIndex;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: '내 페이지',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '홈',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: '설정',
+          ),
+        ],
+        onTap: (index) {
+          refresh(index);
+        },
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(''),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: SquareButton(
+                    buttonText: '카테고리',
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => CategorySelectionPage()),
+                      );
+                    },
+                  ),
+                ),
+                SizedBox(width: 1.0),
+                Expanded(
+                  child: SquareButton(
+                    buttonText: '패스트푸드',
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => MyCategorySelectionPage()),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+            LayerWidget(
+              color: Colors.lightBlue,
+              text: 'Layer 1 내용',
+            ),
+            LayerWidget(
+              color: Colors.lightGreen,
+              text: 'Layer 2 내용',
+            ),
+            LayerWidget(
+              color: Colors.orange,
+              text: 'Layer 3 내용',
+            ),
+            LayerWidget(
+              color: Colors.purple,
+              text: 'Layer 4 내용',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SquareButton extends StatelessWidget {
+  final String buttonText;
+  final VoidCallback onPressed;
+
+  SquareButton({required this.buttonText, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double buttonSize = (screenWidth - 48.0) / 3;
+
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        primary: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        side: BorderSide(color: Colors.black),
+        minimumSize: Size(buttonSize, buttonSize),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(
+          buttonText,
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+    );
+  }
+}
+
+class LayerWidget extends StatelessWidget {
+  final Color color;
+  final String text;
+
+  LayerWidget({required this.color, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      color: color,
+      child: Center(
+        child: Text(text),
+      ),
+    );
+  }
+}
+
+// 추가적인 코드들...
---
+++ lib/pages.dart
@@ -0,0 +1,38 @@
+// 페이지들을 포함한 파일 생성 (pages.dart)
+import 'package:flutter/material.dart';
+
+class MyInfoPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('MyInfoPage'),
+    );
+  }
+}
+
+class SettingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('SettingPage'),
+    );
+  }
+}
+
+class CategorySelectionPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('CategorySelectionPage'),
+    );
+  }
+}
+
+class MyCategorySelectionPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('MyCategorySelectionPage'),
+    );
+  }
+}
